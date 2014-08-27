36cfe709db588abf2993631786e0c2dc8584b2a3
aes215 <aes215@lehigh.edu>
2014-08-27 15:58:10 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//Abby Sprinkel
+//8/27/2014
+//Lab 1: Hello World
+
+
+//define the class
+
+public class HelloWorld{
+    
+    //add main method
+    public static void main(String[] args){
+
+    //create the print statement
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2014!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");    
+    System.out.println("########################");
+    System.out.println("testing");
+    
+    }
+}
