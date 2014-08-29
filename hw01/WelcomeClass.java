c79799f3f164c9444cb194b77d9f13a4e8d8d70d
aes215 <aes215@lehigh.edu>
2014-08-29 15:20:42 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Abby Sprinkel
+//8/27/2014
+//Homework 1- Welcome Class
+
+//Create the class
+
+public class WelcomeClass{
+
+    //add method
+    public static void main(String [] args){
+    
+        //create the print statements
+        
+        System.out.println("-------------");
+        System.out.println("|  Welcome  |");
+        System.out.println("-------------");
+        
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ "); //use the double backslash
+        System.out.println("|-A--E--S--2--1--5-|");       //for special characters
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        
+        System.out.println("I am a senior on the women's varsity field hockey team.");
+        System.out.println("Lift. Laugh. Lufh");
+    }
+}
